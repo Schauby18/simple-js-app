1e08d0e745150e0facabc8c8f8b563c2ead3abab
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-03-27 09:32:14 -0500

    Added 6 pokemon for pokedex app, including name, height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [];
+let pokemonList = [{name:'Squirtle', height:'0.5m', type:'Water'},
+                   {name:'Wartortle', height:'1m', type:'Water'},
+                   {name:'Blastoise', height:'1.6m', type:'Water'},
+                   {name:'Pidgey', height:'0.3m', type:['Flying', 'Normal']},
+                   {name:'Pidgeotto', height:'1.1m', type:['Flying', 'Normal']},
+                   {name:'Pidgeot', height:'1.5m', type:['Flying', 'Normal']}];
