b50626a0993b16f1d6458a34547f015937b051d9
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-03-30 09:49:23 -0500

    Added loop for pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-let pokemonList = [{name:'Squirtle', height:'0.5m', type:'Water'},
-                   {name:'Wartortle', height:'1m', type:'Water'},
-                   {name:'Blastoise', height:'1.6m', type:'Water'},
-                   {name:'Pidgey', height:'0.3m', type:['Flying', 'Normal']},
-                   {name:'Pidgeotto', height:'1.1m', type:['Flying', 'Normal']},
-                   {name:'Pidgeot', height:'1.5m', type:['Flying', 'Normal']}];
+let pokemonList = [{name:'Squirtle', height:'0.5', type:'Water'},
+                   {name:'Wartortle', height:'1', type:'Water'},
+                   {name:'Blastoise', height:'1.6', type:'Water'},
+                   {name:'Pidgey', height:'0.3', type:['Flying', 'Normal']},
+                   {name:'Pidgeotto', height:'1.1', type:['Flying', 'Normal']},
+                   {name:'Pidgeot', height:'1.5', type:['Flying', 'Normal']}];
+
+let pokemonBig = ' - wow, that is big!';
+
+for (let i=0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonBig));}
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + ' (pokemonList[i].height) + ');
+    }    
+    }                   
