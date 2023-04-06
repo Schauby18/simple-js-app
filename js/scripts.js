3e5416fbfdcdc7ef10e264e36803005c2a2b8114
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-04-06 10:43:22 -0500

    Add IIFE to js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
-let pokemonList = [{name:'Squirtle', height:'0.5', type:'Water'},
+let pokemonRepository = (function () {
+  let pokemonList = [{name:'Squirtle', height:'0.5', type:'Water'},
                    {name:'Wartortle', height:'1', type:'Water'},
                    {name:'Blastoise', height:'1.6', type:'Water'},
                    {name:'Pidgey', height:'0.3', type:['Flying', 'Normal']},
                    {name:'Pidgeotto', height:'1.1', type:['Flying', 'Normal']},
                    {name:'Pidgeot', height:'1.5', type:['Flying', 'Normal']}];
 
-let pokemonBig = ' - wow, that is big!';
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  function getAll() {
+    return pokemonList;
+  }
 
-function myLoopFunction(list) {
-    if (list.height > 1.5) {
-        document.write(list.name + ' (height: ' + (list.height) + ')' + (pokemonBig) + ' <br>');}
-    else {
-        document.write(list.name + ' (height: ' + (list.height) + ')<br>');
-    }    
-    }                   
-pokemonList.forEach(myLoopFunction); 
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+                   
+  let pokemonBig = ' - wow, that is big!';
+
+  function myLoopFunction(list) {
+      if (list.height > 1.5) {
+          document.write(list.name + ' (height: ' + (list.height) + ')' + (pokemonBig) + ' <br>');}
+     else   {
+          document.write(list.name + ' (height: ' + (list.height) + ')<br>');
+       }    
+     }                   
+  pokemonRepository.getAll().forEach(myLoopFunction); 
