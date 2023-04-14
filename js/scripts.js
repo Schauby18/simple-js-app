077ea53e1389ccf1cdd29db3e5590decc1937165
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-04-14 12:32:11 -0500

    Clean up code on js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 let pokemonRepository = (function () {
-  let pokemonList = [{name:'Squirtle', height:'0.5', type:'Water'},
-                   {name:'Wartortle', height:'1', type:'Water'},
-                   {name:'Blastoise', height:'1.6', type:'Water'},
-                   {name:'Pidgey', height:'0.3', type:['Flying', 'Normal']},
-                   {name:'Pidgeotto', height:'1.1', type:['Flying', 'Normal']},
-                   {name:'Pidgeot', height:'1.5', type:['Flying', 'Normal']}];
+
+  let pokemonList = [
+    {name:'Squirtle', height:'0.5', type:'Water'},
+    {name:'Wartortle', height:'1', type:'Water'},
+    {name:'Blastoise', height:'1.6', type:'Water'},
+    {name:'Pidgey', height:'0.3', type:['Flying', 'Normal']},
+    {name:'Pidgeotto', height:'1.1', type:['Flying', 'Normal']},
+    {name:'Pidgeot', height:'1.5', type:['Flying', 'Normal']}
+  ];
 
 
   function add(pokemon) {
