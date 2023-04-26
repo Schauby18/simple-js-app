df521edfdfac9e22da3968496425065fcf127fab
Schauby18 <127323987+Schauby18@users.noreply.github.com>
2023-04-25 21:03:28 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,7 @@ let pokemonRepository = (function () {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
+        showModal(item);
       }).catch(function (e) {
         console.error(e);
       });
